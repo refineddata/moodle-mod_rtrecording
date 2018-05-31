9358c92febd39e3e99a5b728019e8c4bda14d397
Elvis <elvis.li@refineddata.com>
2018-05-31 15:10:04 -0400

    RT-347#Adobe Connect Button not working

--- index.php
+++ index.php
@@ -1,44 +1,72 @@
-<?php // $Id: index.php
+<?php
+
 /**
- * This page lists all the instances of a connect Activity in a particular course
+ * This page lists all the instances of rtrecording in a particular course
  *
- * @author  Gary Menezes
- * @version $Id: index.php
- * @package connect
- **/
-    require_once( "../../config.php" );
-    global $CFG, $DB;
+ * @package    mod
+ * @subpackage rtrecording
+ * @copyright  Elvis Li <elvis.li@refineddata.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
-    $id = optional_param( 'id', 0, PARAM_INT );
-    if ( !$course = $DB->get_record( 'course', array( 'id'=>$id ) ) ) print_error( 'Course ID is incorrect-' . $id );
+require_once('../../config.php');
+require_once('lib.php');
 
-    require_login( $course );
-    $event = \mod_connect\event\course_module_instance_list_viewed::create(array(
-    		'context' => context_course::instance($course->id)
-    ));
-    $event->trigger();
-    
+$id = required_param('id', PARAM_INT);           // Course Module ID
 
-    $strtitle       = get_string( "modulenameplural", "rtrecording" );
-    $strname        = get_string( 'name' );
-    
-    $PAGE->set_url( '/mod/rtrecording/index.php', array( 'id'=>$id ) );
-    $PAGE->set_pagelayout( 'incourse' );
-    $PAGE->set_title( $strtitle );
-    $PAGE->set_heading( $course->fullname );
-    $PAGE->navbar->add( $strtitle );
+// Ensure that the course specified is valid
+if (!$course = $DB->get_record('course', array('id'=> $id))) {
+    print_error('Course ID is incorrect');
+}
+
+// Requires a login
+require_course_login($course);
+
+// Declare variables
+$currentsection = "";
+$printsection = "";
+$timenow = time();
+
+// Strings used multiple times
+$strrtrecordings = get_string('modulenameplural', 'rtrecording');
+$strname  = get_string("name");
+$strsectionname = get_string('sectionname', 'format_'.$course->format);
+
+// Print the header
+$PAGE->set_pagelayout('incourse');
+$PAGE->set_url('/mod/rtrecording/index.php', array('id'=>$course->id));
+$PAGE->navbar->add($strrtrecordings);
+$PAGE->set_title($strrtrecordings);
+$PAGE->set_heading($course->fullname);
+
+// Get the rtrecordings, if there are none display a notice
+if (!$rtrecordings = get_all_instances_in_course('rtrecording', $course)) {
     echo $OUTPUT->header();
-
-    if (! $rtrecs = get_all_instances_in_course( "rtconnect", $course ) ) notice( get_string( 'thereareno', 'moodle', $strtitle ), "../../course/view.php?id=$course->id" );
-
-    $table = new html_table();
-
-    foreach ($rtrecs as $rtrec) {
-        $table->data[] = array( '<a href="view.php?a=' . $rtrec->id . '">' . $rtrec->name . '</a>' );
-    }
-    echo '<br />';
-
-    if ( !empty( $table ) ) echo html_writer::table( $table );
-
+    notice(get_string('nortrecordings', 'rtrecording'), "$CFG->wwwroot/course/view.php?id=$course->id");
     echo $OUTPUT->footer();
-?>
+    exit();
+}
+
+$table = new html_table();
+
+$table->head  = array ($strname);
+
+foreach ($rtrecordings as $rtrecording) {
+    if (!$rtrecording->visible) {
+        // Show dimmed if the mod is hidden
+        $link = html_writer::tag('a', $rtrecording->name, array('class' => 'dimmed',
+            'href' => $CFG->wwwroot . '/mod/rtrecording/view.php?id=' . $rtrecording->coursemodule));
+    } else {
+        // Show normal if the mod is visible
+        $link = html_writer::tag('a', $rtrecording->name, array('class' => 'dimmed',
+            'href' => $CFG->wwwroot . '/mod/rtrecording/view.php?id=' . $rtrecording->coursemodule));
+    }
+
+    $table->data[] = array ($link );
+
+}
+
+echo $OUTPUT->header();
+echo '<br />';
+echo html_writer::table($table);
+echo $OUTPUT->footer();
